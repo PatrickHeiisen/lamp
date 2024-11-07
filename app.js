732b7052370f41eaa8b1a56cd5b622a5346b433b
Patrick Heiisen <patrickguimaraescorreia@gmail.com>
2024-11-07 20:42:04 -0300

    lampada

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 /**
- * Simalador de Lampada
+ * Simulador de Lampada
  * @author Patrick G
  */
-// consta
+function quebrar(){
+    document.getElementById('lamp').src="img/broken.jpg"
+    // reproduzindo um arquivo de audio no js
+    // passo 1 -> copiar o arquivo de audio para o projeto
+    // passo 2 -> usar a classe audio (biblioteca interna)
+    let som = new Audio()
+    som.src = "sound/glassbreaking.wav"
+    som.play()
+
+}
